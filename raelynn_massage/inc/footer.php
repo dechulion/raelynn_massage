f152ad05063d272edb97c3ab9d3521be5bb7cc57
dechulion <dechulion@yahoo.com>
2013-10-26 15:16:09 -0700

    Initial Silex run, hooked up to Twig, broke stuff

---
+++ raelynn_massage/inc/footer.php
@@ -0,0 +1,12 @@
+
+</div>
+
+<div class="footer">
+  <div class="container">
+    <div class="copyright">Copyright &copy; Keron Therapeutic Massage <?php echo date('Y');?>  <span>Phone: 360-485-5384</span></div>
+    <a href="#">Contact</a>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ web/form_script.php
@@ -0,0 +1,11 @@
+<?PHP
+
+$name=$_POST['name'];
+$size=$_POST['size'];
+$gender=$_POST['gender'];
+
+print "<p>Name: $name</br>Size: $size</br>Gender: $gender</p>";
+
+
+
+?>
