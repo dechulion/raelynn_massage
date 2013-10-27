87bd07cc6f3611bd4a59370e8ea9e4fc8e558e79
dechulion <dechulion@yahoo.com>
2013-10-26 18:07:20 -0700

    Added rates page, made misc. fixes,
    Add rates page
    Extract page title from twig files
    Pulled php out of layout
    Change margin on top container

--- raelynn_massage/index.php
+++ raelynn_massage/index.php
@@ -5,26 +5,33 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views'
+  'twig.path' => __DIR__.'/views'
 ));
 
-// $title = "Keron Therapeutic Massage";
-// $header = "<div class='col-md-9 title'><h1>Keron Therapeutic Massage</h1> <p>Phone: 360-485-5384</p></div>";
+// Be able to read things under here
 
 $app->before(function () use ($app) {
-    $app['twig']->addGlobal('layout', $app['twig']->loadTemplate('layout.twig'));
+  $app['twig']->addGlobal('layout', $app['twig']->loadTemplate('layout.twig'));
 });
 
+// Write things under here
+
 $app->get('/', function () use ($app) {
-    // if (!isset($home)) {
-    //     $app->abort(404, "Post $id does not exist.");
-    // }
+  // if (!isset($home)) {
+  //     $app->abort(404, "Post $id does not exist.");
+  // }
   return $app['twig']->render('index.html.twig');
 });
 
 $app->get('/about', function () use ($app) {
   return $app['twig']->render('about.html.twig');
 });
+
+$app->get('/rates', function () use ($app) {
+  return $app['twig']->render('rates.html.twig');
+});
+
+// Write things above here
 
 $app->run();
 
