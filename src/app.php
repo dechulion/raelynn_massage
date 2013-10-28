d6198be8b5b14c7c78b72babb1fb175adc9e29cb
dechulion <dechulion@yahoo.com>
2013-10-28 15:49:18 -0700

    Creat pages for Client Forms and Testimonials, add photo for Raelynn, modify some css

--- src/app.php
+++ src/app.php
@@ -16,7 +16,7 @@ $app->before(function () use ($app) {
 
 $app->get('/', function () use ($app) {
   // if (!isset($home)) {
-  //     $app->abort(404, "Post $id does not exist.");
+  //     $app->abort(404, "Page does not exist.");
   // }
   return $app['twig']->render('index.html.twig');
 });
@@ -29,6 +29,13 @@ $app->get('/rates', function () use ($app) {
   return $app['twig']->render('rates.html.twig');
 });
 
+$app->get('/forms', function () use ($app) {
+  return $app['twig']->render('forms.html.twig');
+});
+
+$app->get('/testimonials', function () use ($app) {
+  return $app['twig']->render('testimonials.html.twig');
+});
 // Write things above here
 
 return $app;
